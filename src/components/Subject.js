32430b3671cb32d65922557513e9ffd37eace040
junki-stevekim <junkistevekim@gmail.com>
2022-12-23 13:54:57 +0900

    Components 쪼개기

---
+++ src/components/Subject.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class Subject extends Component{
+    render(){
+      return(
+        // 컴포넌트는 항상 하나의 최상의 태그로 시작한다.
+        <header>  
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+        </header>
+      );
+    }
+  }
+
+  export default Subject;
---
+++ src/components/TOC.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class TOC extends Component{
+    render(){
+      return(
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.css">CSS</a></li>
+          <li><a href="3.javascript">JavaScript</a></li>
+        </ul>
+      );
+    }
+  }
+
+  export default TOC;
