fcf7d206c08d0812bf1dc48cb9e11d06a7a0e650
junki-stevekim <junkistevekim@gmail.com>
2022-12-27 19:38:40 +0900

    Contents 컴포넌트생성, event props 맛보기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import './App.css';
 import TOC from './components/TOC';
 import Subject from './components/Subject';
+import Contents from './components/Contents';
 
 
 // 컴포넌트 생성 코드
@@ -11,17 +12,33 @@ class App extends Component{
   constructor(props){ //constructor 함수는 항상 컴포넌트 최상위에 위치.
     super(props); //state값을 초기화.
     this.state={
-      subject:{title:'React',sub:'World Wide Web!'}
+      mode:'welcome', // 페이지 구분 , 동적페이지 생성
+      subject:{title:'React',sub:'World Wide Web!'},
+      welcome:{title:'Welcome',desc:'Hello React!!'},
+      contents:[
+        {id:1,title:"HTML", desc:"HTML is HyperText...."},
+        {id:2,title:"CSS", desc:"CSS is for Design...."},
+        {id:3,title:"JavaScript", desc:"JavaScript is for interactive...."}
+      ]
     }
   }
-  render(){
+  render(){ //props나 state가 새로고침되면 render함수도 새로 호출된다. 즉 다시 렌더링됨.
+    var _title,_desc=null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       <div className="App">
         <Subject title={this.state.subject.title} 
         sub={this.state.subject.sub}>
 
         </Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
+        <Contents title={_title}  desc={_desc}></Contents>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -5,7 +5,7 @@ class Subject extends Component{
       return(
         // 컴포넌트는 항상 하나의 최상의 태그로 시작한다.
         <header>  
-          <h1>{this.props.title}</h1>
+          <h1><a href="/">{this.props.title}</a></h1>
           {this.props.sub}
         </header>
       );
