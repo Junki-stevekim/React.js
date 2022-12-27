1abefb33fbe826d3c8c3f38b6f7131dc7435cd56
junki-stevekim <junkistevekim@gmail.com>
2022-12-27 11:36:01 +0900

    State 추가

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React,{Component} from 'react';
 import './App.css';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
 
 
 // 컴포넌트 생성 코드
 // 클래스를 생성하고 render라는 Method를 가지고 컴포넌트를 생성한다.
 // 템플릿이라고 생각하자.
 class App extends Component{
+  constructor(props){ //constructor 함수는 항상 컴포넌트 최상위에 위치.
+    super(props); //state값을 초기화.
+    this.state={
+      subject:{title:'React',sub:'World Wide Web!'}
+    }
+  }
   render(){
     return(
       <div className="App">
-        <Subject title="React" sub="For Powerful FE Framework"></Subject>
+        <Subject title={this.state.subject.title} 
+        sub={this.state.subject.sub}>
+
+        </Subject>
         <TOC></TOC>
       </div>
     );
   }
 }
 
-class TOC extends Component{
-  render(){
-    return(
-      <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.css">CSS</a></li>
-        <li><a href="3.javascript">JavaScript</a></li>
-      </ul>
-    );
-  }
-}
-class Subject extends Component{
-  render(){
-    return(
-      // 컴포넌트는 항상 하나의 최상의 태그로 시작한다.
-      <header>  
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
+
+
+
 export default App;
